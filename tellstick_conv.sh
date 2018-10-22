d3eab45f82c8c14c099bd8cfe4f96974b6b117d3
Joakim Lindbom <Joakim.Lindbom@gmail.com>
2018-10-22 08:47:26 +0200

    Clean-up, now the file syntax is compatible with hassio

--- tellstick_conv.sh
+++ tellstick_conv.sh
@@ -5,32 +5,45 @@
 #
 
 FILE='tellstick.conf'
+FILEOUT='tellstick.json'
 
-sed -i '/#/d' $FILE
+rm $FILEOUT 2>/dev/null
+
+sed '/#/d' $FILE > $FILEOUT
 
 # Add comma sign at end of line
-sed -i '/^  id = / s/$/,/g' $FILE
-sed -i '/^  name = / s/$/,/g' $FILE
-sed -i '/^  protocol = / s/$/,/g' $FILE
-sed -i '/^  model = / s/$/,/g' $FILE
-sed -i '/^    house = / s/$/,/g' $FILE
+sed -i '/^  id = / s/$/,/g' $FILEOUT
+sed -i '/^  name = / s/$/,/g' $FILEOUT
+sed -i '/^  protocol = / s/$/,/g' $FILEOUT
+sed -i '/^  model = / s/$/,/g' $FILEOUT
+sed -i '/^    house = / s/$/,/g' $FILEOUT
 
 # Remove
-sed -i 's/device {/{/g' $FILE
-sed -i 's/id = /\"id\": /g' $FILE
+sed -i 's/device {/{/g' $FILEOUT
+sed -i 's/id = /\"id\": /g' $FILEOUT
 
 #Add double quotes
-sed -i 's/name = /\"name\": /g' $FILE
-sed -i 's/  controller = 0//g' $FILE
-sed -i 's/protocol = /\"protocol\": /g' $FILE
-sed -i 's/model = /\"model\": /g' $FILE
-sed -i 's/    house = /\  "house\": /g' $FILE
-sed -i 's/    unit = /\  "unit\": /g' $FILE
-sed -i 's/    code =/\  "code\": /g' $FILE
+sed -i 's/name = /\"name\": /g' $FILEOUT
+sed -i 's/  controller = 0//g' $FILEOUT
+sed -i 's/protocol = /\"protocol\": /g' $FILEOUT
+sed -i 's/model = /\"model\": /g' $FILEOUT
+sed -i 's/    house = /\  "house\": /g' $FILEOUT
+sed -i 's/    unit = /\  "unit\": /g' $FILEOUT
+sed -i 's/    code =/\  "code\": /g' $FILEOUT
 
 #Remove
-sed -i 's/  parameters {//g' $FILE
-sed -i 's/  }//g' $FILE
+sed -i 's/  parameters {//g' $FILEOUT
+sed -i 's/  }//g' $FILEOUT
 
 # Add comma sign
-sed -i 's/}/},/g' $FILE
+sed -i 's/}/},/g' $FILEOUT
+
+#Remove the controller section at the end
+sed -i '/controller {/,$d' $FILEOUT
+
+#tac $FILEOUT | awk '/},/ && ! seen {print "}"; seen=1} {print}' | tac
+sed -zE 's_(.*)},_\1}_' $FILEOUT >tempfile
+mv tempfile $FILEOUT
+#sed 's/\(.*\)},\(.*\)/\1\2/' $FILEOUT
+
+sed -i '/^$/d' $FILEOUT
